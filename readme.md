d60ec80883960e505b184a7684c214c366c6cf3f
Andrew Faurash <andrew2020wit@gmail.com>
2024-01-25 13:01:15 +0200

    add uniter.js

--- readme.md
+++ readme.md
@@ -1,4 +1,7 @@
 # Convert dsl-dictionary to json
 
+See also:
+
+[dictionary.interface.ts](https://github.com/andrew2020wit/bilingua-dealer/blob/master/dictionary/dictionary.interface.ts)
 
 This code is under MIT-license.
---
+++ src/uniter.js
@@ -0,0 +1,61 @@
+const fs = require('node:fs');
+
+const dictionaryName = 'Lingvo-end-Longman';
+const dictionaryTermLanguage = 'en';
+const dictionaryLicense = 'Not licensed';
+const updateDate = new Date().toISOString();
+const formatDescriptor = 'JSONDictionary';
+
+// a source file must be utf-8!
+const sourcePath1 = './dict-source/lingvo.json';
+const sourcePath2 = './dict-source/longman.json';
+
+const dictName1 = 'lingvo';
+const dictName2 = 'longman';
+
+const outputPath = './dict-source/lingvo-and-longman.json';
+
+const source1 = fs.readFileSync(sourcePath1, 'utf-8');
+const source2 = fs.readFileSync(sourcePath2, 'utf-8');
+
+const dict1 = JSON.parse(source1);
+const dict2 = JSON.parse(source2);
+
+const newDictionaryArticles = {};
+
+addDictionary(dict1, dictName1);
+addDictionary(dict2, dictName2);
+
+const newDictionaryToExport = Object.keys(newDictionaryArticles).map(key => {
+	return {
+		term: key.trim(),
+		articles: newDictionaryArticles[key]
+	};
+}
+);
+
+function addDictionary(dict, dictionaryName) {
+	let x = 1;
+	dict.terms.forEach(term => {
+		let currentTerm = term.term;
+		
+		if (currentTerm === 'constructor') {
+			currentTerm = 'constructor '
+		}
+		
+		if (!newDictionaryArticles[currentTerm]) {
+			newDictionaryArticles[currentTerm] = [];
+		}
+		
+		x++;
+		console.log(x, currentTerm);
+
+		term.articles.forEach(art => art.dictionaryName = dictionaryName);
+
+		newDictionaryArticles[currentTerm] = [...(newDictionaryArticles[currentTerm]), ...term.articles];
+	});
+}
+
+console.log('======== Done! ============');
+
+fs.writeFileSync(outputPath, JSON.stringify({ formatDescriptor, dictionaryName, dictionaryTermLanguage, dictionaryLicense, updateDate, terms: newDictionaryToExport }));
