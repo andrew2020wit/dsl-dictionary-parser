79c14de7e59aef7e48f52d8c3176cf47dd485cfa
Andrew Faurash <andrew2020wit@gmail.com>
2024-02-10 15:42:07 +0200

    Transcription

--- src/uniter.js
+++ src/uniter.js
@@ -9,22 +9,27 @@ const formatDescriptor = 'JSONDictionary';
 // a source file must be utf-8!
 const sourcePath1 = './dict-source/lingvo.json';
 const sourcePath2 = './dict-source/longman.json';
+const sourcePath3 = './wordset-dictionary/wordset-with-tr.json';
 
 const dictName1 = 'lingvo';
 const dictName2 = 'longman';
+const dictName3 = 'wordset';
 
-const outputPath = './dict-source/lingvo-and-longman.json';
+const outputPath = './dict-source/lingvo-and-longman-and-wordset.json';
 
 const source1 = fs.readFileSync(sourcePath1, 'utf-8');
 const source2 = fs.readFileSync(sourcePath2, 'utf-8');
+const source3 = fs.readFileSync(sourcePath3, 'utf-8');
 
 const dict1 = JSON.parse(source1);
 const dict2 = JSON.parse(source2);
+const dict3 = JSON.parse(source3);
 
 const newDictionaryArticles = {};
 
 addDictionary(dict2, dictName2);
 addDictionary(dict1, dictName1);
+addDictionary(dict3, dictName3);
 
 const newDictionaryToExport = Object.keys(newDictionaryArticles).map(key => {
 	return {
